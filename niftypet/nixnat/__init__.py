ad15b7acad095e7068301f2b0daf57859fe8260c
Pawel Markiewicz <p.markiewicz@ucl.ac.uk>
2019-07-17 16:52:41 +0100

    added bones and meat

---
+++ niftypet/nixnat/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""initialise the NIXNAT package (part of NiftyPET package)"""
+__author__      = "Pawel Markiewicz"
+__copyright__   = "Copyright 2019 Pawel Markiewicz @ University College London"
+#------------------------------------------------------------------------------
+
+	
+from xnat import xnat
---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+""" XNAT input/output functionalities for namespace package 'niftypet'.
+"""
+__author__      = "Pawel J. Markiewicz"
+__copyright__   = "Copyright 2019"
+# ---------------------------------------------------------------------------------
+
+from setuptools import setup, find_packages
+
+import os
+import sys
+import platform
+
+
+#===============================================================
+# PYTHON SETUP
+#===============================================================
+
+print 'i> found those packages:'
+print find_packages(exclude=['docs'])
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+
+#----------------------------
+setup(
+    name='nixnat',
+    license = 'Apache 2.0',
+    version='1.0.0',
+    description='XNAT input/output.',
+    long_description=long_description,
+    author='Pawel J. Markiewicz',
+    author_email='p.markiewicz@ucl.ac.uk',
+    url='https://github.com/pjmark/NiftyPET',
+    keywords='XNAT input output',
+    install_requires=[],
+    packages=find_packages(exclude=['docs']),
+    zip_safe=False,
+)
+#===============================================================
